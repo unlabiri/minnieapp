34ce83a5ec507d6a3a2211db91d306c1cd6ce387
Jina <bagherijina@gmail.com>
2026-03-31 13:44:23 -0500

    Vocal Template

---
+++ MinnieApp/Vocal_Template.swift
@@ -0,0 +1,18 @@
+//
+//  Vocal_Template.swift
+//  MinnieApp
+//
+//  Created by Jina Bagheri on 3/31/26.
+//
+
+import SwiftUI
+
+struct Vocal_Template: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Vocal_Template()
+}
